13c0daacec32700bb6d4a2bb2421ff6474c5125d
Elijah Cobb <elijah@elijahcobb.com>
2020-02-10 20:21:27 -0500

    base done

--- ts/index.ts
+++ ts/index.ts
@@ -16,11 +16,55 @@ import * as Path from "path";
 	const paperKey: string = paperKeyData.toString("utf8");
 	const bot: KBBot = await KBBot.init("otto_bot", paperKey);
 
-
+	bot.command({
+		name: "pic",
+		description: "Receive a picture.",
+		usage: "!pic",
+		handler: async (message: KBMessage, res: KBResponse): Promise<void> => {
+			await res.sendFile("/home/elijah/Pictures/profile-small.jpeg");
+		}
+	});
 
 	bot.command({
-		name: "x",
+		name: "add",
+		description: "Add all numbers provided together.",
+		usage: "!add 1 12 123 1234",
 		handler: async (message: KBMessage, res: KBResponse): Promise<void> => {
+
+			const nums: (string | number)[] = message.getParameters();
+			let t: number = 0;
+			for (const num of nums) if (typeof num === "number") t += num;
+			await res.send(t);
+
+		}
+	});
+
+	bot.command({
+		name: "multiply",
+		description: "Multiply all numbers provided together.",
+		usage: "!multiply 4 5 6",
+		handler: async (message: KBMessage, res: KBResponse): Promise<void> => {
+
+			const nums: (string | number)[] = message.getParameters();
+			let t: number = 1;
+			for (const num of nums) if (typeof num === "number") t *= num;
+			await res.send(t);
+
+		}
+	});
+
+	bot.command({
+		name: "pow",
+		description: "Compute the power (-p) of a base (-b).",
+		usage: "!pow -b 2 -p 3",
+		parameters: {
+			"b": "number",
+			"p": "number"
+		},
+		handler: async (message: KBMessage, res: KBResponse): Promise<void> => {
+
+			const nums: {b?: number, p?: number} = message.getModifiers();
+			await res.send(Math.pow(nums.b ?? 0, nums.p ?? 0));
 
 		}
 	});
